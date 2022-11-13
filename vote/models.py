b599b463756eb5adf82dbf67518881d252951e78
MellowMancer <yatharth.w@somaiya.edu>
2022-11-13 14:39:32 +0530

    More stylingggggggggggg

--- vote/models.py
+++ vote/models.py
@@ -10,8 +10,7 @@ class ADivPoll(models.Model):
     candidate_1_count = models.IntegerField(default=0)
     candidate_2_count = models.IntegerField(default=0)
     candidate_3_count = models.IntegerField(default=0)
-    
-    #id=models.UUIDField(default=uuid.uuid1, unique=True, primary_key=True, editable=False)
+    #id=models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
     def __str__(self):
         return self.Elections_for
